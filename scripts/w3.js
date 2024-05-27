32b8b5178f42d9475bb2b1da78807f54c36b5a0f
Matiss Bolsteins <bolsteins.matiss@gmail.com>
2024-05-27 14:24:36 +0300

    fizzimg

--- scripts/w3.js
+++ scripts/w3.js
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Define the URL for the Buzz image
     const buzzImageUrl = 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0d7f10a8-159b-4916-9ab4-fae3a5fefb62/de3u5o5-09ccf2e7-bf98-4f60-94f0-39fc1b2d02a1.jpg/v1/fill/w_894,h_894,q_70,strp/buzz_lightyear__toy_story__square_by_alittlecuriousfan99_de3u5o5-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTI4MCIsInBhdGgiOiJcL2ZcLzBkN2YxMGE4LTE1OWItNDkxNi05YWI0LWZhZTNhNWZlZmI2MlwvZGUzdTVvNS0wOWNjZjJlNy1iZjk4LTRmNjAtOTRmMC0zOWZjMWIyZDAyYTEuanBnIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.lg8UNnoYcrxpsqGQrWggWeH_WiVbrMe_LtkhkDxtWaM';
+    const fizzImageUrl = 'https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/ec972080-ce09-4114-86ba-0a7ecc151dcd/width=450/00055-1884010075.jpeg'
 
     const createDivs = () => {
         container.innerHTML = '';
@@ -26,11 +27,15 @@ document.addEventListener('DOMContentLoaded', () => {
                 div.innerText = 'FizzBuzz';
             } else if (i % fizzValue === 0) {
                 div.className = 'fizz';
-                div.innerText = 'Fizz';
+                const img = document.createElement('img');
+                img.src = fizzImageUrl; // use URL
+                img.alt = 'Fizz'
+                // div.innerText = 'Fizz';
+                div.appendChild(img);
             } else if (i % buzzValue === 0) {
                 div.className = 'buzz';
                 const img = document.createElement('img');
-                img.src = buzzImageUrl; // Use the defined URL
+                img.src = buzzImageUrl; // use URL
                 img.alt = 'Buzz';
                 div.appendChild(img);
             } else {
